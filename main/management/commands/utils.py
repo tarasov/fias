0e248f8976c2b31d91216c056348e2e5591e2d6e
Ruslan Tarasov <tarasov@pyata.pro>
2013-06-10 13:56:36 +0400

    fixs

--- main/management/commands/utils.py
+++ main/management/commands/utils.py
@@ -17,11 +17,13 @@ def parse_fias(model, fields, xml_path):
             model.objects.all().delete()
             names = attrs.getNames()
             if names:
-                obj = model(dict((field, attrs._attrs.get(field.upper())) for field in fields))
+                data = dict((field, attrs._attrs.get(field.upper())) for field in fields)
+                print data
+                obj = model(data)
                 bulk_list.append(obj)
                 if len(bulk_list) % 2500 == 0:
-                    print u'commit - {0}'.format(len(bulk_list))
                     model.objects.bulk_create(bulk_list)
+                    print u'commit - {0}'.format(len(bulk_list))
                     bulk_list = []
 
     xml.sax.parse(xml_path, FiasHandler())
